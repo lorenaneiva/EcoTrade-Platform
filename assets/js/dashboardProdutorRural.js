cde616dc1b8827e91d3560184360b2187accbe64
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-10-30 17:42:55 -0300

    dashboard adm scroll animation

---
+++ assets/js/dashboardProdutorRural.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({reset : true });
+
+sr.reveal('.chart', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
+sr.reveal('.content', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
+sr.reveal('.card', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
