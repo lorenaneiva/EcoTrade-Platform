95f7a343058a8aa76a28ba4977746b4f350a399b
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-10-30 16:45:30 -0300

    home: fade animation

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({reset : true });
+
+sr.reveal('.maincontent', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
+sr.reveal('.carousel', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
+sr.reveal('.footer-content', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
