9d7e6c751e7ac020831d6a46d2057b3a4d3e61da
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-10-30 17:03:01 -0300

    add: title on second section

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,10 @@ sr.reveal('.carousel', {
     duration: 2000,
     rotate: {x: 30, y: 0, z:0}
 })
+sr.reveal('.titlesecond', {
+    duration: 2000,
+    rotate: {x: 30, y: 0, z:0}
+})
 sr.reveal('.footer-content', {
     duration: 2000,
     rotate: {x: 30, y: 0, z:0}
